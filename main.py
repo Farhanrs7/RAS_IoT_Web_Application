706cc6d68416af4560bd7e2a2ac01fe3b97cbe11
Farhanrs7 <Farhanrais100@gmail.com>
2024-05-27 23:52:27 +0800

    commented video streaming using ffmpeg

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def create_app():
 application = create_app()
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=5000, debug=True, threaded=True, ssl_context=('./mycert.crt','./mycert.key'))
+    application.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
