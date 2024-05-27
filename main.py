03d1d8ec3a11c9891547428da80b9a3997cba005
Farhanrs7 <Farhanrais100@gmail.com>
2024-05-28 02:54:11 +0800

    added ssl certificates

--- main.py
+++ main.py
@@ -13,4 +13,5 @@ def create_app():
 application = create_app()
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    application.run(host='0.0.0.0', port=5000, threaded=True, ssl_context=('/etc/letsencrypt/live/drfish-server.online/fullchain.pem',
+                                                                           '/etc/letsencrypt/live/drfish-server.online/privkey.pem'))
